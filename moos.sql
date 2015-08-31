c089ddec67802dd8350f70486c027a6355881b81
Tri Tran Dinh <tritd@nal.vn>
2015-08-31 17:04:28 +0700

    Update DB script

--- moos.sql
+++ moos.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: Aug 27, 2015 at 12:30 PM
+-- Generation Time: Aug 31, 2015 at 12:02 PM
 -- Server version: 5.5.42
 -- PHP Version: 5.6.10
 
@@ -23,7 +23,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `administrator` (
   `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
-  `firts_name` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL,
   `first_name_kana` varchar(255) NOT NULL,
   `last_name_kana` varchar(255) NOT NULL,
@@ -36,7 +36,15 @@ CREATE TABLE `administrator` (
   `last_login_time` datetime DEFAULT NULL,
   `created` datetime DEFAULT NULL,
   `updated` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `administrator`
+--
+
+INSERT INTO `administrator` (`id`, `username`, `first_name`, `last_name`, `first_name_kana`, `last_name_kana`, `email`, `password`, `birthday`, `address`, `phone`, `last_login_ip`, `last_login_time`, `created`, `updated`) VALUES
+(1, 'admin', 'administrator 1', 'moos 2', 'first name kana', 'last name kana', 'admin@moos.jp', 'e10adc3949ba59abbe56e057f20f883e', NULL, NULL, NULL, NULL, NULL, NULL, '2015-08-29 05:28:51'),
+(2, 'admin', 'administrator 1', 'moos', 'first name kana', 'last name kana', 'admin@moos.jp', '0192023a7bbd73250516f069df18b500', NULL, NULL, NULL, NULL, NULL, NULL, '2015-08-29 05:53:13');
 
 -- --------------------------------------------------------
 
@@ -48,7 +56,14 @@ CREATE TABLE `approved_rounds` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `description` text
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `approved_rounds`
+--
+
+INSERT INTO `approved_rounds` (`id`, `name`, `description`) VALUES
+(1, 'Restister Basic Info', NULL);
 
 -- --------------------------------------------------------
 
@@ -69,9 +84,17 @@ CREATE TABLE `articles` (
   `updated` datetime DEFAULT NULL,
   `created_by` int(11) DEFAULT NULL,
   `updated_by` int(11) DEFAULT NULL,
-  `is_publish` tinyint(1) NOT NULL DEFAULT '0',
+  `is_published` tinyint(1) NOT NULL DEFAULT '0',
   `view_count` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `articles`
+--
+
+INSERT INTO `articles` (`id`, `title`, `short_content`, `content`, `small_image`, `large_image`, `slug`, `author`, `created`, `updated`, `created_by`, `updated_by`, `is_published`, `view_count`) VALUES
+(2, 'test news', 'short description', 'test\r\ntest\r\ntest test  ', NULL, NULL, '', NULL, '2015-08-28 12:23:09', '2015-08-28 12:23:09', NULL, NULL, 1, 0),
+(3, 'fdsggsfdgsdf gsdf gsfd gg', '123', 'fdsfsgsd fg\r\nsdfg \r\nsdf \r\nsdfg\r\nsdfg\r\nsdf g\r\nfds g\r\n', '2000px-No_Logo_logo1.png', '2000px-No_Logo_logo1.png', '', 'author', '2015-08-28 12:23:24', '2015-08-31 10:26:34', NULL, NULL, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -108,11 +131,18 @@ CREATE TABLE `contacts` (
   `email` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
   `title` varchar(255) NOT NULL,
-  `description` text,
+  `content` text,
   `created` datetime DEFAULT NULL,
   `status` int(11) NOT NULL DEFAULT '1' COMMENT '1: Un-read, 2: Read, 3: Answer',
   `updated` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `contacts`
+--
+
+INSERT INTO `contacts` (`id`, `email`, `name`, `title`, `content`, `created`, `status`, `updated`) VALUES
+(1, 'a', 'bc', 'adfdsaf', 'sdf sadfsda', '2015-08-28 13:28:36', 1, '2015-08-28 13:28:36');
 
 -- --------------------------------------------------------
 
@@ -167,6 +197,9 @@ CREATE TABLE `users` (
 --
 -- Dumping data for table `users`
 --
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `first_name_kana`, `last_name_kana`, `email`, `password`, `date_of_birth`, `day_of_birth`, `month_of_birth`, `year_of_birth`, `genre`, `married_status`, `family_structure`, `created`, `updated`, `approved_round_id`) VALUES
+(1, 'の管理', 'việt nam', 'first name Kana', 'last name kana', 'test@nal.vn', 'fcea920f7412b5da7be0cf42b8c93759', 100, 1, 1, 1990, 'male', 1, 1, '2015-08-27 15:00:45', '2015-08-27 15:00:45', 1);
 
 -- --------------------------------------------------------
 
@@ -322,6 +355,12 @@ ALTER TABLE `administrator`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `approved_rounds`
+--
+ALTER TABLE `approved_rounds`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `articles`
 --
 ALTER TABLE `articles`
@@ -404,10 +443,20 @@ ALTER TABLE `user_relations`
 --
 
 --
+-- AUTO_INCREMENT for table `administrator`
+--
+ALTER TABLE `administrator`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
+--
+-- AUTO_INCREMENT for table `approved_rounds`
+--
+ALTER TABLE `approved_rounds`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
+--
 -- AUTO_INCREMENT for table `articles`
 --
 ALTER TABLE `articles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 --
 -- AUTO_INCREMENT for table `careers`
 --
@@ -422,7 +471,7 @@ ALTER TABLE `cities`
 -- AUTO_INCREMENT for table `contacts`
 --
 ALTER TABLE `contacts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 --
 -- AUTO_INCREMENT for table `family_structures`
 --
@@ -437,7 +486,7 @@ ALTER TABLE `health_insurrances`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 --
 -- AUTO_INCREMENT for table `user_address`
 --
